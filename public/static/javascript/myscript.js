dfc21ab2b1f0371cee6e9814536b19584c29412a
Raul Garcia <rgarciapedrosa@gmail.com>
2023-10-09 16:56:47 +0200

    Adding top bar

---
+++ public/static/javascript/myscript.js
@@ -0,0 +1,8 @@
+function click_icon() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
