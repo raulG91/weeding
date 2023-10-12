78df3914865fd95bb3b95bb73a2b28909dbadfcf
raulG91 <rgarciapedrosa@gmail.com>
2023-10-12 11:44:53 +0200

    Adding google maps

--- public/static/javascript/myscript.js
+++ public/static/javascript/myscript.js
@@ -6,3 +6,22 @@ function click_icon() {
       x.className = "topnav";
     }
   }
+
+  function initMap() {
+    const myLatLng = {
+      lat: 37.06764602661133,
+      lng: -4.326528072357178
+    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 18,
+      center: myLatLng,
+      fullscreenControl: false,
+      zoomControl: true,
+      streetViewControl: false
+    });
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "My location"
+    });
+  }
