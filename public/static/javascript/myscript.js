f40b74d072fe5c3c16ba160170bba2f294c994e5
raulG91 <rgarciapedrosa@gmail.com>
2023-10-22 17:07:07 +0200

    New effects and countdown

--- public/static/javascript/myscript.js
+++ public/static/javascript/myscript.js
@@ -1,27 +1,58 @@
 function click_icon() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function initMap() {
-    const myLatLng = {
-      lat: 37.06764602661133,
-      lng: -4.326528072357178
-    };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: myLatLng,
-      fullscreenControl: false,
-      zoomControl: true,
-      streetViewControl: false
-    });
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "My location"
-    });
-  }
+function initMap() {
+  const myLatLng = {
+    lat: 37.06764602661133,
+    lng: -4.326528072357178
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: myLatLng,
+    fullscreenControl: false,
+    zoomControl: true,
+    streetViewControl: false
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "My location"
+  });
+}
+
+function set_countdown() {
+
+  var countDownDate = new Date("Apr 13, 2024 12:12:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
